b79a707a73e73c4256f6f7f1a717c6e31cf38682
yanyingchiu <3littlemoon3@gmail.com>
2020-07-05 01:23:14 +0800

    fix staticfile path

--- sjwb/Journey_Wishing_Bottle/production_settings.py
+++ sjwb/Journey_Wishing_Bottle/production_settings.py
@@ -5,7 +5,7 @@ DATABASES = {
 	'default': dj_database_url.config(),
 }
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/app/Journey_Wishing_Bottle/trips/static'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
